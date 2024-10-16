8497f911335e1e6445e1475b4133be04c3f1f857
Matthieu Larochelle <mrlaroch@ualberta.ca>
2024-10-15 21:15:54 -0600

    Intial Version of User model class

---
+++ Soroban/app/src/main/java/com/example/soroban/User.java
@@ -0,0 +1,100 @@
+package com.example.soroban;
+
+import android.provider.Settings;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Stores any relevant information that is associated to a user.
+ * @Author: Matthieu Larochelle
+ * @Version: 1.0
+ */
+
+public class User {
+    private final String deviceId;
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private ArrayList<View> views;
+
+    /**
+     * Constructor method for User.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     */
+    public User() {
+        this.deviceId = Settings.Secure.ANDROID_ID;
+    }
+
+    /**
+     * Getter method for User's device Id.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     * @Return: device Id string.
+     */
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    /**
+     * Getter method for User's name.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     * @Return: name string.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter method for User's name.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     * @Param: name string.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Getter method for User's email address.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     * @Return: email address string.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Setter method for User's email address.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     * @Param: email address string.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Getter method for User's phone number.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     * @Return: phone number integer.
+     */
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Setter method for User's phone number.
+     * @Author: Matthieu Larochelle
+     * @Version: 1.0
+     * @Param: phone number integer.
+     */
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
