b83848f55d1aa39711cb3648bafc98513a111810
ayanimranch <ayanimra@ualberta.ca>
2024-10-20 01:17:54 -0600

    Created the User-UI-Dashboard. Added a UserDashboardTest as well. Slightly modified the MainActivity.java : added a button to open the page containing the user dashboard. will remove later.

--- Soroban/app/build.gradle.kts
+++ Soroban/app/build.gradle.kts
@@ -32,12 +32,24 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Unit testing dependencies
     testImplementation(libs.junit)
+
+    // UI testing dependencies
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Add missing Espresso dependencies explicitly
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test:rules:1.5.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation ("androidx.test:runner:1.5.2")
 }
+
+
