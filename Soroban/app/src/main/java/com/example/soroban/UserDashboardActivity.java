953131d65bda4c533d1b563990052e7512444439
ayanimranch <ayanimra@ualberta.ca>
2024-10-25 18:22:17 -0600

    Created the User-UI-Dashboard. Added a UserDashboardTest as well. Slightly modified the MainActivity.java : added a button to open the page containing the user dashboard. will remove later.

--- Soroban/app/src/main/java/com/example/soroban/UserDashboardActivity.java
+++ Soroban/app/src/main/java/com/example/soroban/UserDashboardActivity.java
@@ -1,27 +1,51 @@
+/**
+ * Represents the user's dashboard activity, which is supposed to be the main screen for the users.
+ * Author: Ayan Chaudhry
+ * Resources used: ChatGPT for code assistance
+ * @version 1.0
+ */
+
 package com.example.soroban;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserDashboardActivity extends AppCompatActivity {
+import com.example.soroban.EventRegistrationActivity;
 
+public class UserDashboardActivity extends AppCompatActivity {
+    /**
+     * Called when the activity is first created.
+     * @param savedInstanceState
+     * Author: Ayan Chaudhry
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_activity_dashboard);
+        /**
+         * Set up button actions.
+         * Buttons are: Scan QR Code, Join Waiting List, Manage Facility
+         */
 
-        // Set up button actions
         Button scanQrCode = findViewById(R.id.btn_scan_qr_code);
         Button joinWaitingList = findViewById(R.id.btn_join_waiting_list);
         Button manageFacility = findViewById(R.id.btn_manage_facility);
 
+        /**
+         * Set up button click listeners.
+         */
         scanQrCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Action for Scan QR Code
+
+
+                Intent intent = new Intent(UserDashboardActivity.this, EventRegistrationActivity.class);
+                startActivity(intent); // Redirect to EventRegistrationActivity
             }
         });
 
