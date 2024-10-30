87eaeb58ab5ec69049aacfb3011be9270772c56b
Edwin M <edwinm.manalastas@gmail.com>
2024-10-29 22:28:45 -0600

    Changed QR Code UI (draft) and added camera access/QR code scanning

--- Soroban/app/src/main/java/com/example/soroban/QrCodeScanActivity.java
+++ Soroban/app/src/main/java/com/example/soroban/QrCodeScanActivity.java
@@ -5,19 +5,62 @@
 
 package com.example.soroban;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.soroban.EventRegistrationActivity;
+import com.example.soroban.databinding.ActivityScanQrCodeBinding;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 /**
  * Activity to scan QR code and redirect to EventRegistrationActivity
  */
 public class QrCodeScanActivity extends AppCompatActivity {
 
+    private ActivityScanQrCodeBinding binding;
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if(isGranted) {
+                    showCamera();
+                }
+                else {
+                    //Show why user need this permission
+                }
+            });
+
+    private ActivityResultLauncher<ScanOptions> qrCodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+        } else {
+            setResult(result.getContents());
+        }
+    });
+
+    private void setResult(String contents) {
+        binding.textResult.setText(contents);
+    }
+
+    private void showCamera() {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("Scan QR code");
+        options.setCameraId(0);
+        options.setBeepEnabled(false);
+        options.setBarcodeImageEnabled(true);
+        options.setOrientationLocked(false);
+
+        qrCodeLauncher.launch(options);
+    }
     /**
      * Called when the activity is first created.
      * @param savedInstanceState
@@ -25,23 +68,32 @@ public class QrCodeScanActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan_qr_code);
+        initBinding();
+        initViews();
+    }
 
-        // Find the QR code image
-        ImageView qrCodeImage = findViewById(R.id.qr_code_icon);
-
-        // Set click listener to redirect to EventRegistrationActivity
-        qrCodeImage.setOnClickListener(new View.OnClickListener() {
-            /**
-             * Called when the QR code image is clicked.
-             * @param v
-             */
-            @Override
-            public void onClick(View v) {
-                // Navigate to EventRegistrationActivity
-                Intent intent = new Intent(QrCodeScanActivity.this, EventRegistrationActivity.class);
-                startActivity(intent);
-            }
+    private void initViews() {
+        binding.fab.setOnClickListener(view -> {
+            checkPermissionAndShowActivity(this);
         });
+    }
+
+    private void checkPermissionAndShowActivity(Context context) {
+        if(ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED) {
+            showCamera();
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            Toast.makeText(this, "Camera permission required", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+    }
+
+    private void initBinding() {
+        binding = ActivityScanQrCodeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
