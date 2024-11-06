706789ac26d3ff289a53e0d362d576138886cc8f
Edwin M <edwinm.manalastas@gmail.com>
2024-11-05 19:55:36 -0700

    Fixed errors

--- Soroban/app/src/main/java/com/example/soroban/activity/UserDashboardActivity.java
+++ Soroban/app/src/main/java/com/example/soroban/activity/UserDashboardActivity.java
@@ -11,9 +11,7 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.soroban.FacilityDisplayActivity;
 import com.example.soroban.FireBaseController;
-import com.example.soroban.OrganizerDashboardActivity;
 import com.example.soroban.R;
 import com.example.soroban.adapter.EventArrayAdapter;
 import com.example.soroban.fragment.ViewProfileFragment;
