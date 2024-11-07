38dadfd8892a6684fc9cfe95a871dfd71685edf1
iacatmeow22 <ksli@ualberta.ca>
2024-11-07 00:27:38 -0700

    started some basic admin stuff

---
+++ Soroban/app/src/main/java/com/example/soroban/activity/AdminBrowseEventActivity.java
@@ -0,0 +1,18 @@
+package com.example.soroban.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.soroban.R;
+
+public class AdminBrowseEventActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_browse_events);
+
+    }
+
+}
