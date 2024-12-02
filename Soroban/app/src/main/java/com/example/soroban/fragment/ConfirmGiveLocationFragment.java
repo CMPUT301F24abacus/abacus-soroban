596a062d994b686f81fd7970b662409b8ad29c0b
Aaryan Shetty <44059614+AaryanHazCompter@users.noreply.github.com>
2024-12-01 19:03:38 -0700

    Minor changes to give location fragment (indentation, distance and time of query, spelling)

--- Soroban/app/src/main/java/com/example/soroban/fragment/ConfirmGiveLocationFragment.java
+++ Soroban/app/src/main/java/com/example/soroban/fragment/ConfirmGiveLocationFragment.java
@@ -10,9 +10,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -22,11 +20,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.soroban.FireBaseController;
-import com.example.soroban.R;
-import com.example.soroban.model.Event;
 import com.example.soroban.model.User;
-
-import org.osmdroid.util.GeoPoint;
 
 /**
  * Displays a confirmation dialog for users to allow location access for joining an event's waitlist.
@@ -41,7 +35,7 @@ public class ConfirmGiveLocationFragment  extends DialogFragment {
     private User appUser;
     private FireBaseController fireBaseController;
     private GeolocationListener listener;
-    private ActivityResultLauncher<String> requestPersmissionLauncher;
+    private ActivityResultLauncher<String> requestPermissionLauncher;
     private LocationManager locationManager;
 
     @Override
@@ -54,7 +48,7 @@ public class ConfirmGiveLocationFragment  extends DialogFragment {
             throw new RuntimeException(context + "must implement GeolocationListener");
         }
 
-        requestPersmissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted ->{
+        requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted ->{
             if(isGranted){
                 // App can access geo location
             }else{
@@ -66,11 +60,8 @@ public class ConfirmGiveLocationFragment  extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        View view = getLayoutInflater().inflate(R.layout.user_accept_invitation, null);
-
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setTitle("This event requires your location to join the waitlist. Give location?");
-        builder.setView(view);
         builder.setNegativeButton("No", (dialog, which) -> {
             listener.returnResult(false);
         });
@@ -78,35 +69,37 @@ public class ConfirmGiveLocationFragment  extends DialogFragment {
             // Check if location permissions are on
             locationManager = (LocationManager) getSystemService(getContext(), LocationManager.class);
 
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    // App can use location
-                    // Check if GPS is available
-                    boolean hasGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                // App can use location
+                // Check if GPS is available
+                boolean hasGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-                    LocationListener gpsListener = new LocationListener() {
-                        @Override
-                        public void onLocationChanged(@NonNull Location location) {
-                            // Return User's location
-                            listener.setLocation(location.getLatitude(), location.getLongitude());
-                        }
-                    };
-
-                    if (hasGPS) {
-                        locationManager.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER,
-                                5000,
-                                0F,
-                                gpsListener
-                        );
+                LocationListener gpsListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(@NonNull Location location) {
+                        // Return User's location
+                        listener.setLocation(location.getLatitude(), location.getLongitude());
                     }
+                };
 
-                } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) && shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                    // Prompt user to accept or decline location permissions
-                } else {
-                    // Directly ask for permission
-                    requestPersmissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
-                    requestPersmissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                if (hasGPS) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            10000,
+                            3F,
+                            gpsListener
+                    );
                 }
+
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) &&
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Prompt user to accept or decline location permissions
+            } else {
+                // Directly ask for permission
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
 
             listener.returnResult(true);
         });
